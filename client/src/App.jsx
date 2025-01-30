a0f7a7601c725836c616bbfaba334738a51ac4d7
Christopher Karanja Nganga <christopher.nganga@student.moringaschool.com>
2025-01-30 08:29:24 +0300

    create components and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import React from "react"
-import User from './components/User'
-import Task from './components/Task'
-import Project from './components/Project'
-import UserList from './components/UserList'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Users from './pages/Users';
+import Tasks from './pages/Tasks';
+import Projects from './pages/Projects';
 
-
-const App =() => {
+const App = () => {
   return (
-    <div className="app">
-      <h1>Task Tracker App</h1>
-      <User />
-      <Task/>
-      <Project/>
-      <UserList/>
-    </div>
-  )
-}
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
